203e8ad06de40979460d7c9c73ea3bc25cfa06a4
haohonglong <947923503@qq.com>
2018-05-08 13:48:24 +0800

    modify

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -11,6 +11,7 @@ use yii\helpers\Html;
 
 $this->title = '修改文章';
 $this->params['breadcrumbs'][] = $this->title;
+$posts = json_encode($posts);
 ?>
 <script type="text/javascript">
     <?php $this->beginBlock('js'); ?>
@@ -18,24 +19,43 @@ $this->params['breadcrumbs'][] = $this->title;
         'use strict';
         var System = this;
 
-        function init() {
-            new Vue({
-                el: '#address_menu',
-                data: {
-                    menu:[]
-                },
-                methods: {
-                    content: function (id) {
+        new Vue({
+            el: '#posts',
+            data: {
+                posts:<?=$posts?>
+            },
+            methods: {
 
-                    }
-                },
-                created:function () {
+            },
+            created:function () {
 
-                }
+            }
 
 
-            });
-        }
+        });
+
+        new Vue({
+            el: '#article',
+            data: {
+                title:"<?=$article['title']?>",
+                date:"<?=date('Y-m-d',strtotime($article['cdate']))?>",
+                content:"<?=Html::decode($article['content'])?>",
+            },
+            methods: {
+
+            },
+            created:function () {
+
+            }
+
+
+        });
+        $(function(){
+
+
+
+        });
+
 
 
     });
@@ -43,43 +63,41 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php $this->endBlock(); ?>
 </script>
 <?php $this->registerJs($this->blocks['js'], \yii\web\View::POS_END); //将编写的js代码注册到页面底部  ?>
-<div class="row">
+<div class="row" id="article">
     <div class="col-md-12">
         <div class="panel panel-primary">
             <div class="panel-heading">
-                <h3 class="panel-title"><?=$article['title']?></h3>
+                <h3 class="panel-title">{{title}}</h3>
             </div>
             <div class="panel-body">
-                <p class="text-right">发表于: <?=date('Y-m-d',strtotime($article['cdate']))?></p>
-                <?=Html::decode($article['content'])?>
+                <p class="text-right">发表于: {{date}}</p>
+                {{content}}
             </div>
         </div>
 
     </div>
 </div>
-<?php foreach ($posts as $item):?>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title">回复<?=$item['id'];?></h3>
-    </div>
-    <div class="panel-body">
-        <?=$item['content'];?>
-        <p>日期：<?=$item['date'];?> | </p>
-        <?php if(isset($item['reply']) && is_array($item['reply'])):?>
-        <?php foreach ($item['reply'] as $reply):?>
-            <div class="panel panel-default">
+<div id="posts">
+    <div class="panel panel-default" v-for="item in posts">
+        <div class="panel-heading">
+            <h3 class="panel-title">回复{{item.id}}</h3>
+        </div>
+        <div class="panel-body">
+            {{item.content}}
+            <p>日期：{{item.date}} | </p>
+            <div class="panel panel-default" v-for="reply in item.reply">
                 <div class="panel-heading">
-                    <h3 class="panel-title">回复<?=$reply['id'];?></h3>
+                    <h3 class="panel-title">回复{{reply.id}}</h3>
                 </div>
                 <div class="panel-body">
-                    <?=$reply['content'];?>
-                    <p>日期：<?=$reply['date'];?> | </p>
+                    {{reply.content}}
+                    <p>日期：{{reply.date}} | </p>
+
                 </div>
             </div>
-        <?php endforeach;?>
-        <?php endif;?>
+        </div>
     </div>
 </div>
-<?php endforeach;?>
+
 
 
