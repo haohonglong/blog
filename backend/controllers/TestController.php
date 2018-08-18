d6c4e1da60d973e8128ee437882be3202b57fd8c
haohonglong <947923503@qq.com>
2018-08-18 17:14:39 +0800

    add file of test

---
+++ backend/controllers/TestController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: long
+ * Date: 18/08/2018
+ * Time: 5:04 PM
+ */
+
+namespace backend\controllers;
+
+
+use yii\web\Controller;
+use common\helper\Help;
+
+class TestController extends Controller
+{
+    public function actionIndex()
+    {
+
+        exit;
+    }
+
+}
